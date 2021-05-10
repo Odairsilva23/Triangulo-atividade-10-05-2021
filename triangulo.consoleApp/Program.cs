052c14d880cebc57dce6e7f70385e06adb4c37ff
odair.tiodaia95@gmail.com <odair.tiodaia95@gmail.com>
2021-05-10 15:29:12 -0300

    Add project files.

---
+++ triangulo.consoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangulo.consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo1 = new Triangulo(2, 2, 3);
+            Triangulo triangulo2 = new Triangulo(3, 3, 3);
+            Triangulo triangulo3 = new Triangulo(3, 4, 5);
+            Triangulo triangulo4 = new Triangulo(4, 4, 12);
+
+            Console.Write("Triangulo 1:");
+            Console.Write(triangulo1.ExibirValores());
+            Console.WriteLine(triangulo1.Tipo());
+
+            Console.Write("Triangulo 2: ");
+            Console.Write(triangulo2.ExibirValores());
+            Console.WriteLine(triangulo2.Tipo());
+
+            Console.Write("Triangulo 3: ");
+            Console.Write(triangulo3.ExibirValores());
+            Console.WriteLine(triangulo3.Tipo());
+
+            Console.Write("Triangulo 4: ");
+            Console.Write(triangulo4.ExibirValores());
+            Console.WriteLine(triangulo4.Tipo());
+
+
+            Console.ReadLine();
+        }
+
+ 
+    }
+}
+
---
+++ triangulo.consoleApp/Triangulo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace triangulo.consoleApp
+{
+
+     public class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+    
+
+        public Triangulo(double A, double B, double C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+
+        public string ExibirValores()
+        {
+            return "(" + A + "," + B + "," + C + ")";
+        }
+
+        public bool Validar()
+        {
+            bool validacao = true;
+            if ((A <= 0) || (B <= 0) || (C <= 0))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(" os valores dos lados do triangulo devem ser numeros positivos");
+                Console.ResetColor();
+                return false;
+            }
+            if ((A > (B + C)) || (B > (A + C)) || (C > (A + B)))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(" um lado do triangulo é maior que a soma dos outros dois lados");
+                Console.ResetColor();
+                return false;
+            }
+           else
+            {
+                Console.WriteLine("Triangulo Valido");
+                return(validacao);
+            }
+        }
+
+        public string Tipo()
+        {
+            string tipo = "";
+
+            if (Validar())
+            {
+                if ((A == B) && (B == C))
+                {
+                    tipo = "Equilátero: Todos os lados iguais";
+                }
+                else if ((A == B) || (A == C) || (B == C))
+                {
+                    tipo = "É  um Triangulo Isósceles: Dois lados iguais";
+                }
+                else
+                {
+                    tipo = "É um triangulo Escaleno: Todos os lados diferentes";
+                }
+            }
+            else
+            {
+                tipo = "";
+            }
+            return tipo;
+        }
+    }
+}
